2c169ac8a71255dfd9410fd53b2a19f43f47d637
Wiktor Janecki <j.wiktor05@interia.pl>
2024-08-22 17:56:34 +0200

    Add backend register

--- backend/src/routes/login.rs
+++ backend/src/routes/login.rs
@@ -5,7 +5,7 @@ use jwt_simple::{
 };
 use serde::Deserialize;
 use serde_json::{json, Value};
-use sqlx::query_as;
+use sqlx::{query, query_as};
 use tower_cookies::{Cookie, Cookies};
 use tracing::info;
 
@@ -18,7 +18,47 @@ use crate::{
 use super::{JWTContent, AUTH_TOKEN};
 
 pub fn routes() -> Router<AppState> {
-    Router::new().route("/login", post(login_post))
+    Router::new()
+        .route("/login", post(login_post))
+        .route("/register", post(register_post))
+}
+
+async fn register_post(
+    State(AppState { pool, jwt_key }): State<AppState>,
+    cookies: Cookies,
+    payload: Json<LoginPayload>,
+) -> Result<Json<Value>> {
+    let username = payload.username.to_owned();
+    // check if user exist
+    let res = query!("SELECT * FROM users WHERE username=$1", username)
+        .fetch_optional(&pool)
+        .await
+        .map_err(|_| Error::SQLFail)?;
+    if res.is_some() {
+        return Err(Error::RegisterUserExist);
+    }
+    let password = payload.password.to_owned();
+    let res = query_as!(
+        User,
+        "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *",
+        username,
+        password
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|_| Error::SQLFail)?;
+
+    let jwt_content = JWTContent { id: res.id };
+
+    let claims = Claims::with_custom_claims(jwt_content, Duration::from_mins(20));
+    let token = jwt_key.authenticate(claims).map_err(|_| Error::LoginFail)?;
+
+    cookies.add(Cookie::new(AUTH_TOKEN, token.clone()));
+
+    Ok(Json(json!({
+        "success": true,
+        "token": token
+    })))
 }
 
 #[derive(Deserialize)]
